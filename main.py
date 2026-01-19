af6186b4bb52b7f773ca48b8cb43d8f640e75e66
HellReys <63811690+HellReys@users.noreply.github.com>
2026-01-19 12:19:33 -0500

    Created main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from src.monitor import tail_file
+from src.detector import DetectionEngine
+
+load_dotenv()
+
+def main():
+    log_path = os.getenv("LOG_FILE_PATH")
+    threshold = int(os.getenv("THRESHOLD"))
+
+    print(f"--- Log Based Detection and Security Monitor started ---")
+    print(f"--- File being monitored: {log_path} ---")
+
+    engine = DetectionEngine()
+
+    try:
+        for line in tail_file(log_path):
+            result = engine.analyze(line)
+
+            if result:
+                print(f"DETECTION: {result['rule']} | Source IP: {result['ip']}")
+
+    except KeyboardInterrupt:
+        print("\nThe system was shut down by the user. Stay safe!")
+
+if __name__ == "__main__":
+    main()
