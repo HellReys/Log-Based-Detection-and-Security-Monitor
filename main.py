37802e560a5e371c35bf28e3fa27cca2654e2634
HellReys <63811690+HellReys@users.noreply.github.com>
2026-01-20 12:20:27 -0500

    Changes about main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from src.monitor import tail_file
 from src.detector import DetectionEngine
+from src.notifier import TelegramNotifier
 
 load_dotenv()
 
@@ -13,16 +14,27 @@ def main():
     print(f"--- File being monitored: {log_path} ---")
 
     engine = DetectionEngine()
+    notifier = TelegramNotifier()
 
     try:
         for line in tail_file(log_path):
             result = engine.analyze(line)
 
             if result:
+                # 1
                 print(f"DETECTION: {result['rule']} | Source IP: {result['ip']}")
+                # 2
+                status = notifier.send_alert(result)
+
+                if status:
+                    print("📱 Notification sent successfully.")
+                else:
+                    print("⚠️ Notification failed (check logs).")
 
     except KeyboardInterrupt:
         print("\nThe system was shut down by the user. Stay safe!")
+
+
 
 if __name__ == "__main__":
     main()
